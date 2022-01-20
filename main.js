50eb2d00b72a551b531ac32c2c6306cd380a39f9
Serfer225 <serf0428@gmail.com>
2022-01-20 14:09:00 +0200

    Lesson30

---
+++ main.js
@@ -0,0 +1,19 @@
+// let  = prompt("Сколько тебе лет", 18 )
+// alert (1/0)
+// alert ("2" - 2)
+// alert (2 + "2")
+// let name = "Миша"
+// let surname = "Попов"
+// let age = "14"
+// alert ("My name is " + name + ". " + "My surname is " + surname + ". " + "Me is " + age + ". ")
+// let nameFile = "true"
+// alert (nameFile)
+// let ageUser = 1 > 4
+// alert (ageUser)
+// alert (typeof ageUser)
+// let a = "123"
+// alert (typeof a)
+let a = Number (prompt("Введите первое число"))
+let b = Number (prompt("Введите второе число"))
+let c = Number (prompt("Введите действие"))
+alert (ab)
